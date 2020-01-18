c59a86aeb51af557d60a686f73aca837faa30c65
Yitao Xiong <yitao@bitsighttech.com>
2020-01-17 23:53:24 -0500

    Try to fix a jwt auth bug

--- main/middlewares/token_auth.py
+++ main/middlewares/token_auth.py
@@ -14,23 +14,14 @@ class TokenAuthMiddleware:
         self.inner = inner
 
     def __call__(self, scope):
-        scope["user"] = AnonymousUser()
         try:
             query = parse.parse_qs(scope["query_string"].decode("utf-8"))["jwt"][0]
             if query:
                 try:
                     user_jwt = jwt.decode(query, settings.SECRET_KEY,)
                     scope["user"] = User.objects.get(id=user_jwt["user_id"])
-                except (
-                    InvalidSignatureError,
-                    KeyError,
-                    ExpiredSignatureError,
-                    DecodeError,
-                ):
-                    traceback.print_exc()
-                    pass
                 except Exception as e:
-                    traceback.print_exc()
+                    scope["user"] = AnonymousUser()
 
             return self.inner(scope)
         except:

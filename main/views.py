0c99cfe8b5049b0762e03b462acb23ac0836d503
Yitao Xiong <yitao@bitsighttech.com>
2020-01-18 21:41:16 -0500

    Fixed a bug preventing from generating a user

--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
 
 
 @api_view(["post"])
